c3e657e7e0fe80cab040b9fd5c6989bf0184a8d4
zirohanabusa <103222144+zirohanabusa@users.noreply.github.com>
2026-06-17 06:06:53 +0000

    fix: failed test
    remove test distro

--- test/gogcli/test.sh
+++ test/gogcli/test.sh
@@ -5,7 +5,6 @@ cd "$(dirname "$0")"
 source dev-container-features-test-lib
 
 # Template specific tests
-check "distro" lsb_release -c
 check "copied" [ -f "/usr/local/bin/gog" ]
 check "executable" [ -x "/usr/local/bin/gog" ]
 check "version" /usr/local/bin/gog --version
